73926aaa0f93562f6bceb7efb08aafb66239d3b2
Joshua Duke <dukejoshuae@gmail.com>
2022-01-23 10:13:20 -0500

    First Commit

---
+++ src/courses/Courses.jsx
@@ -0,0 +1,7 @@
+import React from "react";
+
+export default function Courses(){
+    return(
+        <h1>This is the Courses component</h1>
+    )
+}
---
+++ src/semesters/Semesters.jsx
@@ -0,0 +1,8 @@
+import React from "react";
+
+export default function Semesters(){
+    
+    return(
+        <h1>This is the semesters component</h1>
+    )
+}
