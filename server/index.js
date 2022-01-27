05788ae073150226fd00345b3731c4010e2cecca
Joshua Duke <dukejoshuae@gmail.com>
2022-01-26 21:43:37 -0500

    Set up mySQL database connection

--- server/index.js
+++ server/index.js
@@ -1,10 +1,29 @@
+import 'dotenv/config'
 import express from 'express';
 import cors from 'cors';
-
+import mysql from 'mysql';
 const app = express();
+let log = console.log;
+
+app.use(cors());
+ 
+const db = mysql.createConnection({
+    user: process.env.DB_USER,
+    host: process.env.DB_HOST,
+    password: process.env.DB_PASS,
+    database: "superheros"
+})
+
+db.connect((err)=>{
+    if(err) throw err;
+    console.log("Connected");
+})
 
 app.get('/', (req, res)=>{
-    res.send("Hi")
+    db.query("SELECT * FROM Students", (err, rows)=>{
+        if(err) throw err;
+        log("Data: ", rows);
+    })
 })
 
 app.listen(5000, ()=>{
