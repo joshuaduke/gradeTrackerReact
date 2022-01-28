b239a539b9d5109444e678737bed8e80da61ba55
Joshua Duke <dukejoshuae@gmail.com>
2022-01-27 21:27:17 -0500

    Add unique uuid in student model

--- server/models/Student.model.js
+++ server/models/Student.model.js
@@ -1,7 +1,12 @@
-import Sequelize from 'sequelize';
+import Sequelize, { DataTypes } from 'sequelize';
 import db from '../config/database.js';
 
  const Student = db.define('student', {
+    id:{
+        type:DataTypes.UUID,
+        defaultValue: DataTypes.UUIDV4,
+        primaryKey: true
+    },
     firstName:{
         type: Sequelize.STRING
     },
--- server/routes/students.js
+++ server/routes/students.js
@@ -2,6 +2,7 @@ import express from 'express';
 import Student from '../models/Student.model.js';
 const router = express.Router();
 
+//get all students
 router.get('/', (req, res) =>{
     Student.findAll()
         .then(students => {
@@ -10,5 +11,27 @@ router.get('/', (req, res) =>{
         })
         .catch(err => {console.log(err)})
 });
+
+router.get('/add', (req, res)=>{
+    const data = {
+        firstName: "Joshua",
+        lastName: "Duke",
+        email: "joshua@test.com"
+    }
+
+    let { firstName, lastName, email  } = data;
+
+    //insert into table
+    Student.create({
+        firstName,
+        lastName,
+        email
+    })
+        .then(student => res.redirect('/students'))
+        .catch(err => { console.log(err)})
+
+})
+//add a student
+
 
 export default router;
